f9fee12880b253fc5f24a950dd89d899ea82b6e9
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-06 17:05:02 +0000

    commit

--- src/main/java/dev/elma/jpa_hibernate_spring_data/JpaHibernateSpringDataApplication.java
+++ src/main/java/dev/elma/jpa_hibernate_spring_data/JpaHibernateSpringDataApplication.java
@@ -1,13 +1,18 @@
 package dev.elma.jpa_hibernate_spring_data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JpaHibernateSpringDataApplication {
+public class JpaHibernateSpringDataApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JpaHibernateSpringDataApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
