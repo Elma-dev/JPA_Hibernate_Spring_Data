5d09748698daa377a03bfe3a4cfd2dbca65f4749
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-06 16:51:22 +0000

    Initial commit

---
+++ src/main/java/dev/elma/jpa_hibernate_spring_data/JpaHibernateSpringDataApplication.java
@@ -0,0 +1,13 @@
+package dev.elma.jpa_hibernate_spring_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateSpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/elma/jpa_hibernate_spring_data/JpaHibernateSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.elma.jpa_hibernate_spring_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
