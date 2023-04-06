3e286efefc74a2c6603705d6fd709ed866d80825
Elma-div <elmajjodi.abdeljalil@gmail.com>
2023-04-06 17:09:17 +0000

    commit

--- src/main/java/dev/elma/jpa_hibernate_spring_data/JpaHibernateSpringDataApplication.java
+++ src/main/java/dev/elma/jpa_hibernate_spring_data/JpaHibernateSpringDataApplication.java
@@ -1,5 +1,8 @@
 package dev.elma.jpa_hibernate_spring_data;
 
+import dev.elma.jpa_hibernate_spring_data.dao.DaoProduct;
+import dev.elma.jpa_hibernate_spring_data.entities.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaHibernateSpringDataApplication implements CommandLineRunner {
 
+    @Autowired
+    private DaoProduct daoProduct;
     public static void main(String[] args) {
         SpringApplication.run(JpaHibernateSpringDataApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
+        //add Products
+        daoProduct.save(new Product(null,"p1",12,1200));
+        daoProduct.save(new Product(null,"p2",13,1300));
+        daoProduct.save(new Product(null,"p3",14,1400));
+        daoProduct.save(new Product(null,"p4",15,1500));
+        //get All products
     }
 }
